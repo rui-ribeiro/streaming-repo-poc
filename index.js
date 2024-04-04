cd6c3bdede091219adfb32a4fcfd207fd384542f
rui.ribeiro <ruiribeiro360@gmail.com>
2024-04-04 17:08:48 +0100

    initial commit

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.get('/stream', (req, res) => {
     }, 10000);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
